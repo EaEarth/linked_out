f582cc916142c7ecb0d4a6cbead41304012d29cd
Lomidaf <puttipat.ketpupong@gmail.com>
2021-02-09 19:48:25 +0700

    create User entity

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { Connection } from 'typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(),AuthModule,UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly connection: Connection) {}
+}
---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entity';
+import { Repository } from 'typeorm';
+
+export type User = any;
+
+@Injectable()
+export class UsersService {
+    constructor(
+        @InjectRepository(User)
+        private readonly repo: Repository<User>,
+    ){}
+    
+    findOne(inusername: string): Promise<User | undefined> {
+        return this.repo.findOne({username: inusername});
+    }
+
+}
