48d41b0b44d2959ce53f59cd77840429cc60d85c
zuikaru <pavat.tangmo@hotmail.com>
2021-02-09 21:16:42 +0700

    Implement mobile navigation bar title

--- frontend/components/Navigation/NavBar.tsx
+++ frontend/components/Navigation/NavBar.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import {
   Button,
   Form,
@@ -10,13 +11,36 @@ import {
 } from 'react-bootstrap';
 import styles from './NavBar.module.scss';
 
+// Default navigation bar title
+const DEFAULT_NAVBAR_TITLE = 'LinkedOut';
+
+// Navigation bar title mapping
+const navBarTitleMapping: Record<string, string> = {
+  '/test': 'Test',
+  '/': 'Home',
+};
+
 export const NavBar = (props) => {
   const router = useRouter();
+  // Navigation bar title for mobile screen
+  const [navBarTitle, setNavBarTitle] = useState(DEFAULT_NAVBAR_TITLE);
+  // Hook on route change
+  useEffect(() => {
+    if (router.pathname in navBarTitleMapping)
+      setNavBarTitle(navBarTitleMapping[router.pathname]);
+    else setNavBarTitle(DEFAULT_NAVBAR_TITLE);
+  }, [router.pathname]);
+  // Render navigation bar
   return (
     <Navbar bg="light" expand="md" sticky="top" className={styles.navbar}>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Brand href="/" className={styles['navbar-brand']}>
+      <Navbar.Brand
+        href="/"
+        className={`d-none d-md-block ${styles['navbar-brand']}`}>
         LinkedOut
+      </Navbar.Brand>
+      <Navbar.Brand className={`d-md-none ${styles['navbar-brand-mobile']}`}>
+        {navBarTitle}
       </Navbar.Brand>
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
