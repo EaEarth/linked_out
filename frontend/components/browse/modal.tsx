a0fd0873d39c105368cbf88bb1e8d04902c8a78e
sunisagi <sirawitchrocket@gmail.com>
2021-02-24 13:22:03 +0700

    browse fetch from backend

--- frontend/components/browse/modal.tsx
+++ frontend/components/browse/modal.tsx
@@ -1,27 +1,65 @@
-import Head from 'next/head';
+import { useRouter } from 'next/router';
 import React, { useState, useEffect } from 'react';
 import { Col, Container, Jumbotron, Form, Row, Modal } from 'react-bootstrap';
-import DefaultLayout from '../../layouts/Default';
 import Select from 'react-select';
 import Link from 'next/link';
+import axios from 'axios';
 import styles from './modal.module.scss';
 
 export const BrowseModal = (props) => {
-  const [position, setPosition] = useState('');
-  const [jobTypes, setJobTypes] = useState<Array<String>>([]);
-  const [location, setLocation] = useState(null);
-  const [wage, setWage] = useState(null);
+  const [search, setSearch] = useState('');
+  const [jobTypes, setJobTypes] = useState([]);
+  const [province, setProvince] = useState("");
+  const [wage, setWage] = useState('');
+  const [tags, setTags] = useState([]);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/job/tag/index')
+      .then(function (response) {
+        if (response.status === 200) {
+          let tags = response.data;
+          const tempTags = [];
+          for (const tag of tags) {
+            tempTags.push({ value: tag.name, label: tag.name });
+          }
+          setTags(tempTags);
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  }, [props.show]);
 
   const options = [
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' },
-    { value: 'cookie', label: 'Cookies' },
-    { value: 'trash', label: 'Trash' },
+    { value: 'Bangkok', label: 'Bangkok' },
+    { value: 'Chiang Mai', label: 'Chiang Mai' },
+    { value: 'Khon Kaen', label: 'Khon Kaen' },
+    { value: 'Prachuap Khiri Khan', label: 'Prachuap Khiri Khan' },
+    { value: 'Chon Buri', label: 'Chon Buri' },
   ];
 
+  const handleSubmitClick = (e) => {
+    e.preventDefault();
+
+    let hasTag = '';
+    let tempTag = [];
+    if (jobTypes.length > 0) {
+      for (const tag of jobTypes) {
+        tempTag.push(tag.value);
+      }
+      hasTag = `&tags=${tempTag}`;
+    }
+
+    const hasProvince = province == "" ? '' : `&province=${province}`;
+    const hasSearch = search == "" ? '' : `&search=${search}`;
+    const hasSalary = (wage == '' || wage == '0') ? '' : `&lowerBoundSalary=${wage}`;
+    router.push(`/jobs?browse=true${hasSearch}${hasTag}${hasSalary}${hasProvince}`);
+  }
+
   return (
-    <Modal {...props} aria-labelledby="contained-modal-title-vcenter" centered>
+    <Modal show={props.show} onHide={props.onHide} id="browseModal" aria-labelledby="contained-modal-title-vcenter" centered>
       <Modal.Header
         closeButton
         className={`show-grid ${styles['browse-body']}`}>
@@ -35,22 +73,22 @@ export const BrowseModal = (props) => {
             <Col>
               <Form>
                 <Form.Group>
-                  <Form.Label className={styles.label}>Position :</Form.Label>
+                  <Form.Label className={styles.label}>Title & Company :</Form.Label>
                   <Form.Control
                     className="form-control"
                     type="text"
-                    onChange={(e) => setPosition(e.target.value)}
+                    onChange={(e) => setSearch(e.target.value)}
                   />
                 </Form.Group>
 
-                <Form.Group controlId="formLocation">
-                  <Form.Label className={styles.label}>Location</Form.Label>
+                <Form.Group controlId="formProvince">
+                  <Form.Label className={styles.label}>province</Form.Label>
                   <Select
-                    value={location}
+                    value={{ value: province, label: province }}
                     options={options}
                     className="basic-multi-select"
                     classNamePrefix="select"
-                    onChange={(e) => setLocation(e)}
+                    onChange={(e) => setProvince(e.value)}
                   />
                 </Form.Group>
 
@@ -59,8 +97,7 @@ export const BrowseModal = (props) => {
                   <Select
                     isMulti
                     value={jobTypes}
-                    name="colors"
-                    options={jobTypes.length >= 3 ? jobTypes : options}
+                    options={jobTypes.length >= 3 ? jobTypes : tags}
                     className="basic-multi-select "
                     classNamePrefix="select"
                     onChange={(e) => setJobTypes(e)}
@@ -84,6 +121,7 @@ export const BrowseModal = (props) => {
                     width="64px"
                     height="64px"
                     className="my-2 stretched-link mx-auto d-block"
+                    onClick={handleSubmitClick}
                   />
                 </Link>
               </Form>
@@ -94,5 +132,6 @@ export const BrowseModal = (props) => {
     </Modal>
   );
 };
+
 
 export default BrowseModal;
